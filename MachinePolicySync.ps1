fcd7c529bc23a01b93acaf9e4cad0238b3e385ab
Matt Johnson <matthew.w.johnson@gmail.com>
2017-05-15 15:49:22 -0600

    WMI Query Changes

--- MachinePolicySync.ps1
+++ MachinePolicySync.ps1
@@ -12,13 +12,14 @@ Invoke-WmiMethod `
 -Name RequestRefresh -ComputerName $SiteServer
 #$cred = Get-credential 
 #Retrieve SCCM collection by name 
-start-sleep -s 45
+start-sleep -s 4
 $Collection = get-wmiobject -ComputerName $siteServer -NameSpace "ROOT\SMS\site_$SiteCode" -Class SMS_Collection -Credential $cred  | where {$_.Name -eq "$CollectionName"} 
 #Retrieve members of collection 
-$SMSMemebers = Get-WmiObject -ComputerName $SiteServer -Credential $cred -Namespace  "ROOT\SMS\site_$SiteCode" -Query "SELECT resourceid FROM SMS_FullCollectionMembership WHERE CollectionID='$($Collection.CollectionID)'" | select resourceid
+$SMSMemebers = Get-WmiObject -ComputerName $SiteServer -Credential $cred -Namespace  "ROOT\SMS\site_$SiteCode" -Query "SELECT * FROM SMS_FullCollectionMembership WHERE CollectionID='$($Collection.CollectionID)'" | select name
 
 write "There are " $SMSMemebers.Count "objects for a Machine Policy Sync"
 foreach ($SMSMember in $SMSMemebers) {
-    Invoke-WMIMethod -ComputerName $SMSMember -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000021}" -Verbose
-    Invoke-WMIMethod -ComputerName $SMSMember -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000022}" -Verbose
+    $SMSMember
+    Invoke-WMIMethod -ComputerName $SMSMember.name -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000021}" -Verbose
+    Invoke-WMIMethod -ComputerName $SMSMember.name -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule "{00000000-0000-0000-0000-000000000022}" -Verbose
 }
